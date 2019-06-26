c31df8580644c2bded2b5ef9bd09f5d6fe76dbbb
Rob Toombs <rtoombs@shieldsrx.com>
2019-06-26 10:42:44 -0400

    Updated pom.xml

--- src/test/SetTest.java
+++ src/test/SetTest.java
@@ -2,11 +2,12 @@ package test;
 
 import main.Set;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class SetTest {
 
